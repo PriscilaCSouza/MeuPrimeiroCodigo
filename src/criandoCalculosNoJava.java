e32f3a88cde71fbfd6dd162396c97f4862024ffb
PriscilaCSouza <you@example.com>
2022-09-13 20:35:11 -0300

    Evolução do meu primeiro codigo

---
+++ src/criandoCalculosNoJava.java
@@ -0,0 +1,13 @@
+
+public class criandoCalculosNoJava {
+
+	public static void main(String[] args) {
+		// Criando Calculos no Java
+		int valor1=100, valor2=200;
+		int resultado=valor1+valor2;
+		
+		System.out.println("O 1º valor é "+valor1+", o 2º valor é "+valor2+", o resultado obtido na soma é "+resultado+".");
+		
+	}
+
+}
---
+++ src/mostrarMensagemDeBoasVindas.java
@@ -0,0 +1,17 @@
+
+public class mostrarMensagemDeBoasVindas {
+
+	public static void main(String[] args) {
+		// Criando uma mensagem para aparecer na tela
+		System.out.println("Priscila, seja bem vinda!");
+		System.out.println("Hoje é a sua 1ª Aula de Desenvolvimento Java :)");
+		System.out.println();
+		
+		// Criando variáveis em Java
+		String nome= "Priscila Souza";
+		int idade=30;
+		System.out.println("Olá "+nome+", sua idade é "+idade+" anos!");
+				
+	}
+
+}
